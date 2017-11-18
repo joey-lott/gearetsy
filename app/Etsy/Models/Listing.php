8f490c3ed27ed35508a44770fdbe20951285a488
joey-lott <joeylott@gmail.com>
2017-11-17 19:29:44 -0500

    updated who made setting for listings

--- app/Etsy/Models/Listing.php
+++ app/Etsy/Models/Listing.php
@@ -13,7 +13,7 @@ class Listing {
   public $price;
   public $taxonomy_id;
   public $tags;
-  public $who_made = "i_did";
+  public $who_made = "someone_else";
   public $when_made = "made_to_order";
   public $state = "draft";
   public $is_supply = "false";
